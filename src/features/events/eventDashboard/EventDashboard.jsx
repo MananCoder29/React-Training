40fac5515d023629dde62ad4171e17e59a850eac
MananCoder29 <81809248+MananCoder29@users.noreply.github.com>
2023-05-11 18:29:53 +0530

    initial commit

---
+++ src/features/events/eventDashboard/EventDashboard.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { Grid } from "semantic-ui-react";
+import EventList from "./EventList";
+// import { sampleData } from "../../../app/api/SampleData";
+import { useSelector } from "react-redux";
+
+export default function EventDashboard() {
+  // const [events, setEvents] = useState(sampleData);
+  const {events} = useSelector(state => state.event);
+
+  // function handleCreateEvent(event) {
+  //   setEvents([...events, event]);
+  // }
+
+  // function handleUpdateEvent(updatedEvent) {
+  //   setEvents(events.map(evt => evt.id === updatedEvent.id ? updatedEvent : evt));
+  // }
+
+  // function handleDeleteEvent(eventId) {
+  //   setEvents(events.filter(evt => evt.id !== eventId));
+  // }
+
+  return (
+    <Grid>
+      <Grid.Column width={10}>
+        <EventList events={events}
+         /*deleteEvent={handleDeleteEvent}*/ />
+      </Grid.Column>
+      <Grid.Column width={6}>
+        <h2>Event Filters</h2>
+      </Grid.Column>
+    </Grid>
+  );
+}
